0b8ec0fa24438d8c761996561f9b2348f74e873c
israel <zatur.pc@gmail.com>
2023-08-19 00:03:44 -0600

    agregados archivos -_-

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Israel Portillo - Request/Response Cycle</title>
+    </head>
+    <body>
+        <h1>Israel Portillo Request / Response</h1>
+        <p>The SHA256 hash of "Israel Portillo" is <?php
+        print hash('sha256', 'Israel Portillo');
+?>
+</p>
+<pre>
+ASCII ART:
+
+            
+         ___   
+        /\  \  
+        \:\  \ 
+        /::\__\
+     __/:/\/__/
+    /\/:/  /   
+    \::/__/    
+     \:\__\    
+      \/__/    
+            
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
